1c820a752cbc7374723574d96c009a7db5f88d5d
Breyton Pabst <breytonpabst@gmail.com>
2023-09-30 17:22:16 -0600

    added url functionality

--- makeText.js
+++ makeText.js
@@ -2,14 +2,33 @@
 
 const {MarkovMachine} = require("./markov");
 const fs = require("fs");
+const axios = require("axios");
 
 
-fs.readFile("eggs.txt", "utf8", (err, data) => {
-    if (err){
-        console.log(err.message);
+if (process.argv[2] === "file"){
+    fs.readFile(process.argv[3], "utf8", (err, data) => {
+        if (err){
+            console.log(err.message);
+            process.exit(1);
+        }
+        const test = new MarkovMachine(data);
+        console.log(test.makeText());
+        process.exit(0);
+    });
+}
+else if (process.argv[2] === "url"){
+    axios.get(process.argv[3])
+    .then(({data}) => {
+        const test = new MarkovMachine(data);
+        console.log(test.makeText());
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.log(err);
         process.exit(1);
-    }
-    const test = new MarkovMachine(data);
-    console.log(test.makeText());
-    process.exit(0);
-});
+    })
+}
+else {
+    console.log("Invalid selector");
+    process.exit(1);
+}
